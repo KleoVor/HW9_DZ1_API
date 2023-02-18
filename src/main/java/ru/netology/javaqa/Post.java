80f42a6467604cca49794b0abdd8a774f34aaaf6
KleoVor <t9671253993@gmail.com>
2023-02-18 13:50:58 +0300

    added methods Post & FormDate

---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.javaqa;
+
+public class Post {
+    public String name; // поле для имени
+    // добавьте другие поля
+    public String passport; //поле для паспорта
+    public String patronymic; // поле дя отчества
+    public String phone; //поле для тлф
+    public String surname; // поля для фамилии
+    public boolean subscription; // согласие на рассылку по смс
+    public FormDate birthday; // дата рождения
+}
